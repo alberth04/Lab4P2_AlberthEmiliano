be52bff2b430840c6d463b5191ecd65f22f55a7b
alberth04 <godoyalberth.2002@gmail.com>
2022-08-12 16:09:06 -0600

    Empezando con las validacion de movimiento de las piezas

--- src/lab4p2_albertthgodoy_emiliano_agurcia/Peon.java
+++ src/lab4p2_albertthgodoy_emiliano_agurcia/Peon.java
@@ -15,7 +15,7 @@ public class Peon extends Pieza{
 
     @Override
     public boolean validarMovimiento() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
