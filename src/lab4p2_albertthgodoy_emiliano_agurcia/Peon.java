d757fc026f88f00f60768ead37d546f724558c21
Emiliano-Agurcia <110260889+Emiliano-Agurcia@users.noreply.github.com>
2022-08-12 13:32:29 -0600

    Empezando a crear las clases

---
+++ src/lab4p2_albertthgodoy_emiliano_agurcia/Peon.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_albertthgodoy_emiliano_agurcia;
+
+/**
+ *
+ * @author emili
+ */
+public class Peon extends Pieza{
+    
+}
