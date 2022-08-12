c1b861b3c1e1de589c048484f6b567c5a5ce3d2d
alberth04 <godoyalberth.2002@gmail.com>
2022-08-12 13:13:56 -0600

    Empezando el laboratorio

---
+++ src/lab4p2_albertthgodoy_emiliano_agurcia/Lab4P2_AlbertthGodoy_Emiliano_Agurcia.java
@@ -0,0 +1,17 @@
+package lab4p2_albertthgodoy_emiliano_agurcia;
+
+/**
+ *
+ * @author Alberth
+ * @author Emiliano
+ */
+public class Lab4P2_AlbertthGodoy_Emiliano_Agurcia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Comenzando el proyecto
+    }
+    
+}
