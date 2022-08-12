90df130ebd2a8b9228a68a35424905b3a1c0a5e4
alberth04 <godoyalberth.2002@gmail.com>
2022-08-12 13:43:40 -0600

    Trabajando la clase pieza

--- src/lab4p2_albertthgodoy_emiliano_agurcia/Lab4P2_AlbertthGodoy_Emiliano_Agurcia.java
+++ src/lab4p2_albertthgodoy_emiliano_agurcia/Lab4P2_AlbertthGodoy_Emiliano_Agurcia.java
@@ -11,7 +11,7 @@ public class Lab4P2_AlbertthGodoy_Emiliano_Agurcia {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Comenzando el proyecto
+        
     }
     
 }
